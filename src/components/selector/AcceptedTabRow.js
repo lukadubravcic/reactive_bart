4deac95d7eb379b79cff2c7360d457e5e0ba4135
Luka Dubravcic <Luka@Teas-Mac-mini.local>
2017-10-02 18:25:57 +0200

    accepted row

---
+++ src/components/selector/AcceptedTabRow.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AcceptedTabRow = props => {
+    
+    return (
+        <div className="container">
+            {/* stuktura tog reda u tablici */}
+            <span>{props}</span>
+            <span>{}</span>
+            <span>{}</span>
+            <span>{}</span>
+            <span>{}</span>
+        </div>
+    )
+
+}

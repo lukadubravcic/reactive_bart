3a3677e28992b27452b622b492b4323b781d98c7
Luka Dubravcic <Luka@Teas-Mac-mini.local>
2018-05-16 12:46:18 +0200

    new banner and api server

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,4 +2,6 @@ export const ITEMS_PER_PAGE = 10;
 
 export const IGNORED_TIME = 30; // 30 days
 
-export const API_ROOT = /* 'http://localhost:8000' // */  'http://www.lukabart.dreamhosters.com:8000';
+// export const API_ROOT = 'http://localhost:8000';   
+// export const API_ROOT = 'http://www.lukabart.dreamhosters.com:8000';
+export const API_ROOT = 'ps600619.dreamhostps.com:8000';

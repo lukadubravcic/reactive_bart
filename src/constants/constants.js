16d870424334f7213c1017bf1b847a3f713a7de8
Luka Dubravcic <Luka@Teas-Mac-mini.local>
2018-06-19 14:36:59 +0200

    new api root

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,8 +4,9 @@ export const ERR_DISPLAY_TIME = 5000;
 
 export const IGNORED_TIME = 30; // 30 days
 
-export const APP_LINK = 'http://localhost:3000';
+// export const APP_LINK = 'http://localhost:3000';
+export const APP_LINK = 'https://www.skolded.com';
 
-export const API_ROOT = 'http://localhost:8000';   
+// export const API_ROOT = 'http://localhost:8000';   
 // export const API_ROOT = 'http://www.lukabart.dreamhosters.com:8000';
-// export const API_ROOT = 'https://www.skolded.com:8000';
+export const API_ROOT = 'https://www.skolded.com/api';
